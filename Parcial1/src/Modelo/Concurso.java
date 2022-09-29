2ce0c79ee2f0bfd20fc4a59f907a2fc1b6384dab
Chacky37 <113044126+Chacky37@users.noreply.github.com>
2022-09-29 07:56:12 -0500

    Add files via upload

---
+++ Parcial1/src/Modelo/Concurso.java
@@ -0,0 +1,31 @@
+
+package Modelo;
+import java.util.ArrayList;
+public class Concurso {
+    
+    private Llanta info;
+    private ArrayList<Participante>datos;
+    
+    public Concurso() {
+       
+        this.datos=new ArrayList();
+    }
+
+    public Concurso(ArrayList<Participante> datos) {
+        this.datos = datos;
+    }
+    
+
+        
+       
+        
+    }
+    public void Ganador(){
+        System.out.println("Ganador es: ");
+        
+        
+    }
+   }
+   
+    
+
